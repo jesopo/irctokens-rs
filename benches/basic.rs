f262cbc463c5c76930b04e06670333c1eb326c40
jesopo <github@lolnerd.net>
2023-03-21 22:17:05 +0000

    benchmarx

---
+++ benches/basic.rs
@@ -0,0 +1,13 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use irctokens::tokenise;
+
+fn basic() {
+    tokenise(b"@tag1=tag1value;tag2=;tag3 :source COMMAND arg1 arg2 :arg3 with space").unwrap();
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("basic", |b| b.iter(basic));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
